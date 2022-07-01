da433d31a1480c4bce759bf60c3873d1e31bdb05
Israel da Rosa Barcelos <israelbarcelos7@gmail.com>
2022-07-01 01:45:39 -0300

    alterando para br.com.alura.comex

--- src/main/java/br/com/alura/comex/comex/ComexApplication.java
+++ src/main/java/br/com/alura/comex/comex/ComexApplication.java
@@ -1,4 +1,4 @@
-package com.alura.comex.comex;
+package br.com.alura.comex.comex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
